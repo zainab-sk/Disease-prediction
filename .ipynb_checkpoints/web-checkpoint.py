6724eb76a5507f20ecab0b26cb3c9e0668ea64cd
Zainab-Shaikh <shaikhzainab19032001@gmail.com>
2025-02-14 17:41:30 +0530

    Removed unnecessary files

--- .ipynb_checkpoints/web-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import os
-import pickle
-import streamlit as st 
-from streamlit_option_menu import option_menu
-
-st.set_page_config(page_title='Prediction of Disease Outbreak',
-                   layout='wide',
-                   page_icon='👩🏽‍⚕️'
-                   )
